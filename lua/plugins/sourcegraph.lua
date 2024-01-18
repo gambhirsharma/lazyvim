000cf476b85b02c582651e388c3072e53449be68
Gambhir ⚡ <gambhir786191ss@gmail.com>
2024-01-18 14:16:39 +0530

    add: inline code suggestion cody

--- lua/plugins/sourcegraph.lua
+++ lua/plugins/sourcegraph.lua
@@ -1,5 +1,5 @@
 return {
-  "sourcegraph/sg.nvim",
+  {"sourcegraph/sg.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim", --[[ "nvim-telescope/telescope.nvim ]]
   },
@@ -7,6 +7,19 @@ return {
     require("sg").setup({
       enable_cody = true,
       accept_tos = true,
+      event = "InsertEnter",
     })
   end,
+  },
+  {
+{
+  "hrsh7th/nvim-cmp",
+  dependencies = { "sourcegraph/sg.nvim" },
+  ---@param opts cmp.ConfigSchema
+  opts = function(_, opts)
+    local cmp = require("cmp")
+    opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "cody" } }))
+  end,
+}
+  }
 }
