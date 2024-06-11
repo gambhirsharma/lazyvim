d46285779fd10dc176e42cbacc37f36bc46a86da
Gambhir ⚡ <gambhir786191ss@gmail.com>
2024-06-12 01:05:21 +0530

    fix: luasnip
    changed the config and use the lazyvim luasnip example config

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,6 +1,40 @@
 return {
   "L3MON4D3/LuaSnip",
-  dependencies = { "mlaursen/vim-react-snippets" },
+  build = (not LazyVim.is_win())
+      and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
+      or nil,
+  dependencies = {
+    {
+      'mlaursen/vim-react-snippets',
+      config = function()
+        require("vim-react-snippets").lazy_load()
+      end,
+    },
+    {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+      end,
+    },
+    {
+      "nvim-cmp",
+      dependencies = {
+        "saadparwaiz1/cmp_luasnip",
+      },
+      opts = function(_, opts)
+        opts.snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        }
+        table.insert(opts.sources, { name = "luasnip" })
+      end,
+    },
+  },
+  opts = {
+    history = true,
+    delete_check_events = "TextChanged",
+  },
   config = function()
     local ls = require("luasnip")
 
@@ -9,58 +43,6 @@ return {
     ls.filetype_extend("javascriptreact", { "javascript" })
     ls.filetype_extend("vue", { "javascript" })
 
-    require("luasnip.loaders.from_vscode").lazy_load()
     require("config.snip")
-    require("vim-react-snippets").lazy_load()
-
-    local M = {}
-
-    function M.expand_or_jump()
-      if ls.expand_or_jumpable() then
-        ls.expand_or_jump()
-      end
-    end
-
-    function M.jump_next()
-      if ls.jumpable(1) then
-        ls.jump(1)
-      end
-    end
-
-    function M.jump_prev()
-      if ls.jumpable(-1) then
-        ls.jump(-1)
-      end
-    end
-
-    function M.change_choice()
-      if ls.choice_active() then
-        ls.change_choice(1)
-      end
-    end
-
-    function M.reload_package(package_name)
-      for module_name, _ in pairs(package.loaded) do
-        if string.find(module_name, "^" .. package_name) then
-          package.loaded[module_name] = nil
-          require(module_name)
-        end
-      end
-    end
-
-    function M.refresh_snippets()
-      ls.cleanup()
-      M.reload_package("config.snip")
-    end
-
-    local set = vim.keymap.set
-
-    local mode = { "i", "s" }
-    local normal = { "n" }
-
-    set(mode, "<c-i>", M.expand_or_jump)
-    set(mode, "<c-n>", M.jump_prev)
-    -- set(mode, "<c-l>", M.change_chuice)
-    set(normal, ",r", M.refresh_snippets)
-  end,
+  end
 }
